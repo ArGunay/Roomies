0c527dbf227170667d7e7079325d02bec2d4c34e
NiccoloZuppichini <zuppin@usi.ch>
2017-12-01 11:58:47 +0100

    created a really really basic app.js (is not doing anything)

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const app = express()
+
+//TODO 
+
+//connect to mongoDB
+
+
+//routes 
+
+
+
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
