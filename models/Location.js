ee51f33049276d3484aac375c164ef625ca20b5a
volkll <luka@lukas-mbp.mobile.usilu.net>
2017-12-12 11:16:52 +0100

    Updated the location schema, adapted the main-page accordingly

--- models/Location.js
+++ models/Location.js
@@ -4,35 +4,30 @@ const Schema = mongoose.Schema;
 // Verify mail: https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
 
 const LocationSchema = exports.LocationSchema = new Schema({
-    administrator: { 
+    administrator: {
         type: String,
         required: true,
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
 
-    nation: { 
-        type: String, 
-        required: true 
-    },
-
-    city: { 
+    city: {
         type: String,
         required: true
     },
 
-    postalCode: { 
+    postalCode: {
         type: String,
         required: true
     },
 
-    street: { 
+    street: {
         type: String,
         required: true
     },
 
     buildingNumber: { type: String },
 
-    description: { 
+    description: {
         type: String,
         required: true
     },
@@ -42,7 +37,12 @@ const LocationSchema = exports.LocationSchema = new Schema({
         required: true
     },
 
-    rooms: {
+    size: {
+      type: Number,
+      required: true
+    },
+
+    bedrooms: {
         type: Number,
         required: true
     },
@@ -59,9 +59,7 @@ const LocationSchema = exports.LocationSchema = new Schema({
 
     accept_smokers: { type: Boolean },
     accept_animals: { type: Boolean },
-    gender: { type: String },
-    price_min: { type: Number },
-    price_max: { type: Number }
+    gender: { type: String }
 });
 
 mongoose.model('Location', LocationSchema);
