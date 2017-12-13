56c9641e0a7eaf9aeb9f026cdee5bf753b585438
NiccoloZuppichini <zuppin@usi.ch>
2017-12-13 15:38:14 +0100

    now u can send emails

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const config = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+mailer = require('express-mailer');
+
 
 const mongoose = require('mongoose');
 
@@ -24,12 +26,43 @@ const roomie = require("./routes/roomie/router");
 app.use("/location", location);
 app.use("/roomie", roomie);
 
-// app.use(function (err, req, res, next) {
-//   console.log("next is called")
-//     res.status(err.status || 500);
-//     console.log(err.status)
 
-//     res.sendStatus(err.status || 500);})
+
+//send email 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+
+
+mailer.extend(app, {
+from: 'nick.zup@example.com',
+host: 'smtp.gmail.com', // hostname 
+secureConnection: true, // use SSL 
+port: 465, // port for secure SMTP 
+transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts 
+auth: {
+user: 'roomies777@gmail.com',
+pass: 'passwordperlaemail'
+}
+});
+
+app.get('/email', function (req, res, next) {
+    app.mailer.send('email', {
+      to: 'ardil.guenay@usi.ch', // REQUIRED. This can be a comma delimited string just like a normal email to field.  
+      subject: 'You have won the lottery', // REQUIRED. 
+      otherProperty: 'Other Property' // All additional properties are also passed to the template as local variables. 
+    }, function (err) {
+      if (err) {
+        // handle error 
+        console.log(err);
+        res.send('There was an error sending the email');
+        return;
+      }
+      res.send('Email Sent');
+    });
+  });
+
+
 
 
 app.listen(config.port, () => console.log('listening on port 8081'))
