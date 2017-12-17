b6dbbd3763b3146e280927e4e9a29347ba8a6bd3
volkll <luka@Lukas-MacBook-Pro.local>
2017-12-17 22:55:08 +0100

    Added maps for individual locations

--- routes/points/router.js
+++ routes/points/router.js
@@ -16,6 +16,28 @@ var options = {
 // "city":"Lugano","postalCode":"6900","street":"Strada","buildingNumber":"145"
 var geocoder = NodeGeocoder(options);
 
+router.get('/:id', (req, res) => {
+  // console.log("the id is " + req.params.id)
+  Location.findById(req.params.id, function(err, found){
+  	if (err) throw (err);
+    if(found !== null) {
+      console.log(found.buildingNumber + " " + found.street);
+
+      geocoder.geocode({
+        address: found.buildingNumber + " " + found.street,
+        country: "Switzerland",
+        zipcode: found.postalCode
+        },function(err, r) {
+          if(err) throw err;
+          console.log(r);
+          let latLon = {lon : r[0].longitude, lat : r[0].latitude};
+          console.log(latLon)
+          res.status(200).json(latLon);
+        });
+      }
+    });
+});
+
 router.get('/', (req, res) => {
     let q = Location.find({});
     q.select('city postalCode street buildingNumber');
@@ -39,9 +61,6 @@ router.get('/', (req, res) => {
             res.json(ll);
         })
     })
-    
-
-
 });
 
 
