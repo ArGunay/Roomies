312ee87ede6fb3a1268549301c172dd27f1e1578
volkll <luka@Lukas-MacBook-Pro.local>
2017-12-19 11:13:24 +0100

    Fixed maps bug

--- routes/points/router.js
+++ routes/points/router.js
@@ -16,26 +16,30 @@ var options = {
 // "city":"Lugano","postalCode":"6900","street":"Strada","buildingNumber":"145"
 var geocoder = NodeGeocoder(options);
 
-router.get('/:id', (req, res) => {
-  // console.log("the id is " + req.params.id)
-  Location.findById(req.params.id, function(err, found){
-  	if (err) throw (err);
-    if(found !== null) {
-      console.log(found.buildingNumber + " " + found.street);
+router.post('/query', (req, res) => {
+  let filter = req.body;
+  let q = Location.find(filter);
 
-      geocoder.geocode({
-        address: found.buildingNumber + " " + found.street,
-        country: "Switzerland",
-        zipcode: found.postalCode
-        },function(err, r) {
-          if(err) throw err;
-          console.log(r);
-          let latLon = {lon : r[0].longitude, lat : r[0].latitude};
-          console.log(latLon)
-          res.status(200).json(latLon);
-        });
-      }
-    });
+  q.select('city postalCode street buildingNumber');
+
+  q.exec(function (err, locations) {
+      let p = []
+      locations.forEach(function(l){
+          p.push(geocoder.geocode(`${l.street} ${l.buildingNumber}, ${l.city}`))
+      })
+
+      Promise.all(p)
+      .then(function(data){
+          let ll = [];
+          data.forEach(function(l){
+              ll.push({
+                  lon : l[0].longitude,
+                  lat : l[0].latitude
+              })
+          })
+          res.json(ll);
+      })
+  })
 });
 
 router.get('/', (req, res) => {
